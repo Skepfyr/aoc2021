ea2f6601b7a71c9511ea9ee97d16032c8518550e
Jack Rickard <jack.rickard@outlook.com>
2021-12-03 08:39:56 +0000

    Day3 use Ints

--- src/Day03.kt
+++ src/Day03.kt
@@ -9,56 +9,48 @@ fun main() {
 }
 
 class Day03(test: Boolean) {
-    private val input =
-        readInput(3, test).map { line ->
-            line.toCharArray().map {
-                when (it) {
-                    '0' -> false
-                    '1' -> true
-                    else -> throw IllegalArgumentException()
-                }
-            }.toBooleanArray()
-        }
+    private val bits: Int
+    private val values: List<Int>
+
+    init {
+        val input = readInput(3, test)
+        bits = input[0].length
+        values = input.map { it.toInt(2) }
+    }
 
 
     fun part1(): Int {
-        val counts = input.fold(IntArray(input[0].size)) { counts, input ->
-            for (i in input.indices) {
-                counts[input.size - i - 1] += if (input[i]) 1 else 0
-            }
-            counts
-        }
-        val (gamma, epsilon) = counts.withIndex().fold(Pair(0, 0)) { (gamma, epsilon), (i, count) ->
-            val newGamma = gamma + if (count > input.size / 2) 1 shl i else 0
-            val newEpsilon = epsilon + if (count < input.size / 2) 1 shl i else 0
-            Pair(newGamma, newEpsilon)
+        var gamma = 0
+        var epsilon = 0
+        for (bit in 0 until bits) {
+            val numOnes = values.count { it and (1 shl bit) != 0 }
+            val mostCommon = if (numOnes > values.size / 2) 1 else 0
+            val leastCommon = if (numOnes <= values.size / 2) 1 else 0
+            gamma = gamma or (mostCommon shl bit)
+            epsilon = epsilon or (leastCommon shl bit)
         }
         return gamma * epsilon
     }
 
     fun part2(): Int {
-        var o2Reports = input
-        var o2Index = 0
+        var o2Reports = values
+        var o2Bit = bits - 1
         while (o2Reports.size > 1) {
-            val count = o2Reports.count { it[o2Index] }
-            val mostCommon = count in (o2Reports.size + 1) / 2..o2Reports.size
-            o2Reports = o2Reports.filter { it[o2Index] == mostCommon }
-            o2Index += 1
+            val numOnes = o2Reports.count { it and (1 shl o2Bit) != 0 }
+            val mostCommon = numOnes in (o2Reports.size + 1) / 2..o2Reports.size
+            o2Reports = o2Reports.filter { (it and (1 shl o2Bit) != 0) == mostCommon }
+            o2Bit -= 1
         }
-        val o2 = o2Reports[0].reversed().withIndex().fold(0) { value, (i, bit) ->
-            value + if (bit) 1 shl i else 0
-        }
-        var co2Reports = input
-        var co2Index = 0
+        val o2 = o2Reports[0]
+        var co2Reports = values
+        var co2Bit = bits - 1
         while (co2Reports.size > 1) {
-            val count = co2Reports.count { it[co2Index] }
-            val mostCommon = count in 0..(co2Reports.size - 1) / 2
-            co2Reports = co2Reports.filter { it[co2Index] == mostCommon }
-            co2Index += 1
+            val numOnes = co2Reports.count { it and (1 shl co2Bit) != 0 }
+            val mostCommon = numOnes in 0..(co2Reports.size - 1) / 2
+            co2Reports = co2Reports.filter { (it and (1 shl co2Bit) != 0) == mostCommon }
+            co2Bit -= 1
         }
-        val co2 = co2Reports[0].reversed().withIndex().fold(0) { value, (i, bit) ->
-            value + if (bit) 1 shl i else 0
-        }
+        val co2 = co2Reports[0]
         return o2 * co2
     }
 }
